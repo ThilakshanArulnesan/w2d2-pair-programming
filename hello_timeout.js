86dc86ff56790f1bb460ef3064da65059335a80c
ThilakshanArulnesan <th_thilakshan@hotmail.com>
2019-09-24 16:23:45 +0000

    added setTimeout

---
+++ hello_timeout.js
@@ -0,0 +1,14 @@
+console.log("43llo")
+
+setTimeout(() => {
+  console.log("th3r3");
+  setTimeout(() => {
+    console.log("w0r1d");
+  }, 1000);
+}, 1000);
+
+
+
+const printOut = function(str) {
+  console.log(str);
+}
